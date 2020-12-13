2745f6dc4d903e8e6272aeace526362849276e0b
guilhermerodriguesti <57836353+guilhermerodriguesti@users.noreply.github.com>
2020-12-13 19:07:35 -0300

    Update bootstrap.sh

--- user-data/bootstrap.sh
+++ user-data/bootstrap.sh
@@ -3,6 +3,6 @@ yum install -y httpd
 service httpd start
 chkconfig httpd on
 yum install -y stress
-stress -c 1 --vm 2
+#stress -c 1 --vm 2
 ## Number 1
 echo "Deploy com terraform realizado com Sucesso!" > /var/www/html/index.html
